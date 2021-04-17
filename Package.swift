e19c7d376fe4c62441ae13930e958ff5d2de98c2
mr-elbe5 <miro@elbe5.de>
2021-04-17 15:31:10 +0200

    log 1.0.5

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/mr-elbe5/SwiftyHttpServer", from: "1.1.17"),
-        .package(url: "https://github.com/apple/swift-crypto", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-crypto", from: "1.0.0"),
+        .package(url: "https://github.com/mr-elbe5/SwiftyLog", from: "1.0.5")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
