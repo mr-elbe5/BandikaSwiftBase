366c0673622aee9d1694be504fdc7e6838e47e34
mr-elbe5 <miro@elbe5.de>
2022-07-29 13:05:54 +0200

    Create ImageMagick.swift

---
+++ Sources/BandikaSwiftBase/Source/Common/ImageMagick.swift
@@ -0,0 +1,38 @@
+//
+//  ImageMagick.swift
+//  ImageMagickTest
+//
+//  Created by Michael Rönnau on 29.07.22.
+//
+
+import Foundation
+
+public struct ImageMagick{
+    
+    public static var magickPath = "/opt/homebrew/bin/magick"
+    
+    @discardableResult
+    public static func copy(path: String, from: String, to: String) -> Bool{
+        return copy(from: path + from, to: path + to)
+    }
+    
+    @discardableResult
+    public static func copy(from: String, to: String) -> Bool{
+        return run(args: [
+            from,
+            to
+        ])
+    }
+    
+    @discardableResult
+    public static func run(args : [String]) -> Bool{
+        let task = Process()
+        task.launchPath = magickPath
+        task.arguments = args
+        task.launch()
+        task.waitUntilExit()
+        return task.terminationStatus == 0
+    }
+    
+}
+    
